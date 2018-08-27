bb88597395178bc86e15016c582190698a536746
Jeffrey Walton <noloader@gmail.com>
2018-08-26 20:50:49 -0400

    Add check for APP_STL and c++_shared

--- Application.mk
+++ Application.mk
@@ -25,7 +25,16 @@ CRYPTOPP_ROOT := $(call my-dir)
 NDK_PROJECT_PATH := $(CRYPTOPP_ROOT)
 APP_BUILD_SCRIPT := $(CRYPTOPP_ROOT)/Android.mk
 
+GREP ?= grep
+NDK_r16_OR_LATER := $(shell $(GREP) -i -c -E "Pkg.Revision = (1[6-9]|[2-9][0-9]\.)" "$$ANDROID_NDK_ROOT/source.properties")
+ifneq ($(NDK_r16_OR_LATER),0)
+  ifneq ($(APP_STL),c++_shared)
+    $(info Crypto++: NDK r16 or later. Use c++_shared instead of $(APP_STL))
+  endif
+endif
+
 ifeq ($(NDK_LOG),1)
+    $(info Crypto++: ANDROID_NDK_ROOT is $(ANDROID_NDK_ROOT))
     $(info Crypto++: NDK_PROJECT_PATH is $(NDK_PROJECT_PATH))
     $(info Crypto++: APP_BUILD_SCRIPT is $(APP_BUILD_SCRIPT))
 endif
