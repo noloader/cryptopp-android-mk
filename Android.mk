05698bd124702d7d8062ed2108e3b2c9f887ce75
Jeffrey Walton <noloader@gmail.com>
2019-04-10 20:14:24 -0400

    Prepare for Crypto++ 8.0 release

--- Android.mk
+++ Android.mk
@@ -49,30 +49,34 @@ endif
 
 CRYPTOPP_SRC_FILES := \
     cryptlib.cpp cpu.cpp integer.cpp \
-    3way.cpp adler32.cpp algebra.cpp algparam.cpp arc4.cpp \
-    aria-simd.cpp aria.cpp ariatab.cpp asn.cpp authenc.cpp base32.cpp \
-    base64.cpp basecode.cpp bfinit.cpp blake2-simd.cpp blake2.cpp \
+    3way.cpp adler32.cpp algebra.cpp algparam.cpp arc4.cpp aria.cpp \
+    aria_simd.cpp ariatab.cpp asn.cpp authenc.cpp base32.cpp base64.cpp \
+    basecode.cpp bfinit.cpp blake2.cpp blake2b_simd.cpp blake2s_simd.cpp \
     blowfish.cpp blumshub.cpp camellia.cpp cast.cpp casts.cpp cbcmac.cpp \
-    ccm.cpp chacha.cpp channels.cpp cmac.cpp crc-simd.cpp crc.cpp \
-    default.cpp des.cpp dessp.cpp dh.cpp dh2.cpp dll.cpp dsa.cpp eax.cpp \
-    ec2n.cpp eccrypto.cpp ecp.cpp elgamal.cpp emsa2.cpp eprecomp.cpp \
-    esign.cpp files.cpp filters.cpp fips140.cpp fipstest.cpp gcm-simd.cpp \
-    gcm.cpp gf256.cpp gf2_32.cpp gf2n.cpp gfpcrypt.cpp gost.cpp gzip.cpp \
-    hex.cpp hmac.cpp hrtimer.cpp ida.cpp idea.cpp iterhash.cpp kalyna.cpp \
-    kalynatab.cpp keccak.cpp luc.cpp mars.cpp marss.cpp md2.cpp md4.cpp \
-    md5.cpp misc.cpp modes.cpp mqueue.cpp mqv.cpp nbtheory.cpp neon-simd.cpp \
-    network.cpp oaep.cpp osrng.cpp padlkrng.cpp panama.cpp pkcspad.cpp \
-    poly1305.cpp polynomi.cpp ppc-simd.cpp pssr.cpp pubkey.cpp queue.cpp \
-    rabin.cpp randpool.cpp rc2.cpp rc5.cpp rc6.cpp rdrand.cpp rdtables.cpp \
-    rijndael-simd.cpp rijndael.cpp ripemd.cpp rng.cpp rsa.cpp rw.cpp \
-    safer.cpp salsa.cpp scrypt.cpp seal.cpp seed.cpp serpent.cpp \
-    sha-simd.cpp sha.cpp sha3.cpp shacal2-simd.cpp shacal2.cpp shark.cpp \
-    sharkbox.cpp simon-simd.cpp simon.cpp skipjack.cpp sm3.cpp sm4.cpp \
-    socketft.cpp sosemanuk.cpp speck-simd.cpp speck.cpp square.cpp \
-    squaretb.cpp sse-simd.cpp strciphr.cpp tea.cpp tftables.cpp \
-    threefish.cpp tiger.cpp tigertab.cpp trdlocal.cpp ttmac.cpp \
-    tweetnacl.cpp twofish.cpp vmac.cpp wait.cpp wake.cpp whrlpool.cpp \
-    xtr.cpp xtrcrypt.cpp zdeflate.cpp zinflate.cpp zlib.cpp
+    ccm.cpp chacha.cpp chacha_avx.cpp chacha_simd.cpp cham.cpp cham_simd.cpp \
+    channels.cpp cmac.cpp crc.cpp crc_simd.cpp darn.cpp default.cpp des.cpp \
+    dessp.cpp dh.cpp dh2.cpp dll.cpp donna_32.cpp donna_64.cpp donna_sse.cpp \
+    dsa.cpp eax.cpp ec2n.cpp eccrypto.cpp ecp.cpp elgamal.cpp emsa2.cpp \
+    eprecomp.cpp esign.cpp files.cpp filters.cpp fips140.cpp fipstest.cpp \
+    gcm.cpp gcm_simd.cpp gf256.cpp gf2_32.cpp gf2n.cpp gfpcrypt.cpp gost.cpp \
+    gzip.cpp hc128.cpp hc256.cpp hex.cpp hight.cpp hmac.cpp hrtimer.cpp \
+    ida.cpp idea.cpp iterhash.cpp kalyna.cpp kalynatab.cpp keccak.cpp \
+    keccakc.cpp lea.cpp lea_simd.cpp luc.cpp mars.cpp marss.cpp md2.cpp \
+    md4.cpp md5.cpp misc.cpp modes.cpp mqueue.cpp mqv.cpp nbtheory.cpp \
+    neon_simd.cpp oaep.cpp osrng.cpp padlkrng.cpp panama.cpp pkcspad.cpp \
+    poly1305.cpp polynomi.cpp ppc_power7.cpp ppc_power8.cpp ppc_power9.cpp \
+    ppc_simd.cpp pssr.cpp pubkey.cpp queue.cpp rabbit.cpp rabin.cpp \
+    randpool.cpp rc2.cpp rc5.cpp rc6.cpp rdrand.cpp rdtables.cpp \
+    rijndael.cpp rijndael_simd.cpp ripemd.cpp rng.cpp rsa.cpp rw.cpp \
+    safer.cpp salsa.cpp scrypt.cpp seal.cpp seed.cpp serpent.cpp sha.cpp \
+    sha3.cpp sha_simd.cpp shacal2.cpp shacal2_simd.cpp shark.cpp \
+    sharkbox.cpp simeck.cpp simeck_simd.cpp simon.cpp simon128_simd.cpp \
+    simon64_simd.cpp skipjack.cpp sm3.cpp sm4.cpp sm4_simd.cpp sosemanuk.cpp \
+    speck.cpp speck128_simd.cpp speck64_simd.cpp square.cpp squaretb.cpp \
+    sse_simd.cpp strciphr.cpp tea.cpp tftables.cpp threefish.cpp tiger.cpp \
+    tigertab.cpp ttmac.cpp tweetnacl.cpp twofish.cpp vmac.cpp wake.cpp \
+    whrlpool.cpp xed25519.cpp xtr.cpp xtrcrypt.cpp zdeflate.cpp zinflate.cpp \
+    zlib.cpp
 
 #####################################################################
 # Test source files
@@ -80,11 +84,11 @@ CRYPTOPP_SRC_FILES := \
 # Remove adhoc.cpp from this list
 
 CRYPTOPP_TEST_FILES := \
-    test.cpp bench1.cpp bench2.cpp \
-    datatest.cpp dlltest.cpp fipsalgt.cpp \
-    validat0.cpp validat1.cpp validat2.cpp \
-    validat3.cpp validat4.cpp regtest1.cpp \
-    regtest2.cpp regtest3.cpp
+    test.cpp bench1.cpp bench2.cpp bench3.cpp datatest.cpp \
+    dlltest.cpp fipsalgt.cpp validat0.cpp validat1.cpp validat2.cpp \
+    validat3.cpp validat4.cpp validat5.cpp validat6.cpp validat7.cpp \
+    validat8.cpp validat9.cpp validat10.cpp regtest1.cpp regtest2.cpp \
+    regtest3.cpp regtest4.cpp
 
 #####################################################################
 # ARM A-32 source file
