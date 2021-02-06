38fd92bc63c6e65a9f918925d5e332d58c33038a
Jeffrey Walton <noloader@gmail.com>
2021-02-06 01:57:54 -0500

    Prepare for Crypto++ 8.2 tag

--- Android.mk
+++ Android.mk
@@ -111,7 +111,6 @@ endif
 ifeq ($(TARGET_ARCH),arm64)
     CRYPTOPP_LIB_FILES := $(filter-out %avx.cpp,$(CRYPTOPP_LIB_FILES))
     CRYPTOPP_LIB_FILES := $(filter-out sse_simd.cpp,$(CRYPTOPP_LIB_FILES))
-    CRYPTOPP_LIB_FILES := $(filter-out donna_32.cpp,$(CRYPTOPP_LIB_FILES))
     CRYPTOPP_LIB_FILES := $(filter-out donna_sse.cpp,$(CRYPTOPP_LIB_FILES))
 endif
 
